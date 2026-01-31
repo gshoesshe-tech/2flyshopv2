63a9705ea24dc81718c6161de521069d0ebf6f35
gshoesshe-tech <gshoesshe@gmail.com>
2026-01-31 08:05:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,6 @@
 //    - Copy anon public key
 // 2) Paste them below, save, then redeploy to Netlify.
 
-window.__SUPABASE_URL__ = "https://jtzctoormmsouzhgcmzm.supabase.co";
-window.__SUPABASE_ANON_KEY__ = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0emN0b29ybW1zb3V6aGdjbXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyNzk0OTIsImV4cCI6MjA4MDg1NTQ5Mn0.1u_wjYYjHg1a0A7COlS8KysXYpxMZc1SI-liapkf7NY";
+window.__SUPABASE_URL__ = "https://ngthitqzqtnvmsthwddl.supabase.co";
+window.__SUPABASE_ANON_KEY__ = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndGhpdHF6cXRudm1zdGh3ZGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzNjQ5NTEsImV4cCI6MjA4NDk0MDk1MX0.MSC3lSHfazA4vpbM69lwzkzxpws7aiGAQk8oVPTZXNw";
+
